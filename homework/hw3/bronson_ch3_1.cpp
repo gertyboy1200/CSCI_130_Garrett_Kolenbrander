adfbb3008d4b56e65901785eaa79ac59029ba640
gertyboy1200 <garrett.kolenbrander@gmail.com>
2022-09-06 13:37:30 -0600

    bronson_ch3_1🏁

--- homework/hw3/bronson_ch3_1.cpp
+++ homework/hw3/bronson_ch3_1.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main() {
 
-    int x1, x2, y1, y2;
-
-    cout << " input your coordinates [ x1 y1] [x2 y2]: ";
+    double x1, x2, y1, y2;
+    double slope;
+    // input the coordinates and return them 
+    cout << " input your coordinates [ x1 y1] [x2 y2]: "; 
     cin >> x1 >> y1 >> x2 >> y2;
-    cout << "your points were: (" << x1 << " , " << y1 << ") , (" << x2 << " , " << y2 << ")";
+    cout << "your points entered: (" << x1 << " , " << y1 << ") , (" << x2 << " , " << y2 << ")" << endl;
+
+    if (x1 == x2) {
+        cout << "THE SLOPE IS VERTICAL";
+        return 0;
+    }
+
+    // finding the slope
+    slope = ((y2 - y1) / (x2 - x1));
+    cout << setprecision(8) << "the value of the slope is: "<< slope << endl;
+
 
 }
