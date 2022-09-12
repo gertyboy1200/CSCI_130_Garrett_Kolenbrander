9e48aea70bc46f24b7459683cab1fde995e44494
gertyboy1200 <garrett.kolenbrander@gmail.com>
2022-09-11 20:57:38 -0600

    hw3🚧

---
+++ homework/hw3/bronson_ch3_9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main () {
+    double E, R, RC, t, i, e_pow;
+    double e;
+    e = 2.71828;
+    E = 20;
+    R = 10;
+    RC = 0.044;
+    t =0.023;
+    e_pow = pow(e,(-t/RC));
+    i = ((E/R)*e_pow);
+    cout << i;
+}
--- homework/hw3/spavanac.cpp
+++ homework/hw3/spavanac.cpp
@@ -1,28 +1,22 @@
 #include <iostream>
-
 using namespace std;
 
 int main () {
-    
     int H, M;
     cin >> H >> M;
-    
     if (M < 45 && H > 0) {
         M = M - 45;
         M = M + 60;
         H--;
         cout << H << " " << M;
-
     } else if (M >= 45) {
         M = M - 45;
         cout << H << " " << M;
-
     } else if (M < 45 && H == 0) {
         M = M - 45;
         M = M + 60;
         H = 23;
         cout << H << " " << M;
-
     }
     return 0;
 
