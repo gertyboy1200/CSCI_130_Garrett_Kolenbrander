5a1938ecb58e0650f50d38cee863dbaf7bc697de
gertyboy1200 <garrett.kolenbrander@gmail.com>
2022-09-13 14:20:47 -0600

    hw4

---
+++ homework/hw4/last_factorial_digit.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int Ln, N, last_dig;
+    int number, factorial = 1;
+    cin >> Ln;
+    int fact = 1, i;
+    while (Ln > 0) {
+        cin >> N;
+        for(i=1; i<=N; i++)
+        fact = fact * i;
+        cout <<fact << endl;
+        Ln--;
+        fact = 1;
+   }
+
+
+}
