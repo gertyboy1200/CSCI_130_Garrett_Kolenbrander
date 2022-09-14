96669b8bd91b04760d3d9cf1fc57fcf345684d3a
gertyboy1200 <garrett.kolenbrander@gmail.com>
2022-09-14 09:12:08 -0600

    hw4

---
+++ homework/hw4/bronson_ch4_3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "input the numbers a then b: ";
+    cin >> a >> b;
+
+    if ( a > b) {
+        cout << "the first number is greater!!";
+        return 0;
+    } else if (a < b) {
+        cout << "the second number is greater!!";
+        return 0;
+    } else{
+        cout << "the numbers are equal";
+    }
+}
