b300ccdc1fc22d4b89b06f7121b8528b5629b1f7
gertyboy1200 <garrett.kolenbrander@gmail.com>
2022-09-16 11:01:54 -0600

    hw4🏁
    final files for hw4

--- homework/hw4/human_cannonball.cpp
+++ homework/hw4/human_cannonball.cpp
@@ -3,23 +3,23 @@
 using namespace std;
 
 int main() {
-    double v0, theta, x1, h1, h2, N, t, v0x, yt, cos_theta, sin_theta, xc, xs, part_1, part_2;
+    double v0, theta, x1, h1, h2, N, t, v0x, yt, cos_theta, sin_theta, xc, xs;
     const double g = 9.81;
     cin >> N;
     while (N > 0) {
         cin >> v0 >> theta >> x1 >> h1 >> h2;
+        
         xc = theta*3.14159/180;
         cos_theta = cos(xc);
+        
         v0x = (v0 * (cos_theta));
         t = x1 / v0x;
+        
         xs = theta*3.14159/180;
         sin_theta = sin(xs);
-        part_1 = v0 * t * sin_theta;
-        part_2 = 0.5 * g  * pow(t, 2);
-        cout << "part 1: " << part_1 << " part 2: " << part_2 << endl;
+        
         yt = ((v0 * t * sin_theta) - (0.5 * (g * pow(t, 2))));
-        cout << "t: " << t << endl;
-        cout << "yt: " << yt << "h1: " << h1 << "h2: " << h2 << endl;
+
         if ((yt >= (h1 + 1)) && (yt <= (h2 - 1))) {
             cout << "Safe" << endl;
         } else {
--- homework/hw4/last_factorial_digit.cpp
+++ homework/hw4/last_factorial_digit.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main() {
     int Ln, N, last_dig;
     int fact = 1, i;
-    cin >> Ln;
+    cin >> Ln >> N;
     while (Ln > 0) {
-        cin >> N;
         for(i=1; i<=N; i++)
         fact = fact * i;
         fact = fact % 10;
         cout << fact << endl;
+        cin >> N;
         Ln--;
         fact = 1;
    }
