d83b3a1487a6ba44df757c2cdd34aedc6fb79e1a
gertyboy1200 <garrett.kolenbrander@gmail.com>
2022-09-16 09:27:37 -0600

    hw4🚧

--- homework/hw4/human_cannonball.cpp
+++ homework/hw4/human_cannonball.cpp
@@ -9,17 +9,17 @@ int main() {
     while (N > 0) {
         cin >> v0 >> theta >> x1 >> h1 >> h2;
         xc = theta*3.14159/180;
-        cos_theta = cos(x);
+        cos_theta = cos(xc);
         cout << "result: " << cos_theta << endl;
         v0x = (v0 * (cos_theta));
         cout << "v0x: " << v0x << endl;
         t = x1 / v0x;
         cout << "t: " << t << endl;
         xs = theta*3.14159/180;
-        sin_theta = cos(x);
-        yt = ((v0 * t * sin(theta)) - (0.5 * (g * pow(t, 2))));
+        sin_theta = cos(xs);
+        yt = ((v0 * t * sin_theta) - (0.5 * (g * pow(t, 2))));
         cout << "yt: " << yt << " h1: " << h1 << " h2: " << h2 << endl;
-        if ((yt >= (h1 + 1)) && (yt <= (h2 - 2))) {
+        if ((yt >= (h1 + 1)) && (yt <= (h2 - 1))) {
             cout << "Safe" << endl;
         } else {
             cout << "Not Safe" << endl;
