b7ae40ad0beddd83eccbbbe575572940ff2203c3
gertyboy1200 <garrett.kolenbrander@gmail.com>
2022-09-25 23:38:40 -0600

    hw5🏁

--- homework/hw5/bronson_hw5_3.cpp
+++ homework/hw5/bronson_hw5_3.cpp
@@ -1,70 +1,73 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+
 using namespace std;
 
 int main() {
+
     double k1, k2 ,k3, ta, ws, WCI;
 
     k1 = 13.12;
     k2 = 11.37;
     k3 = 0.3965;
-    // WCI = K1 + 0.6125 Ta - K2Ws0.16 + K3TaWs0.16
+
     cout << "temperature (degrees C)" << setfill(' ') << setw(25) << "wind speed (km/h)" << setfill(' ') << setw(15) << "final temp with wind chill (degrees C)" << endl;
-    ta = 2;
-    ws = 5;
-    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
-    
-    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
 
     ta = 2;
-    ws = 7;
-    
-    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
-    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
-    
-    ta = 2;
-    ws = 9;
-    
-    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
-    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
-
-    ta = 2;
-    ws = 11;
-    
-    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
-    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
-    
-    ta = 6;
-    ws = 5;
-    
-    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
-    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
-    
-    ta = 6;
-    ws = 7;
-    
-    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
-    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
-
-    ta = 6;
-    ws = 9;
-    
-    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
-    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
-
-    ta = 6;
-    ws = 11;
-    
-    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
-    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
-
-    ta = 10;
     ws = 5;
 
     WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
     cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
 
+    ta = 2;
+    ws = 7;
+
+    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
+    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
+
+    ta = 2;
+    ws = 9;
+
+    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
+    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
+
+    ta = 2;
+    ws = 11;
+
+    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
+    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
+
+    ta = 6;
+    ws = 5;
+
+    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
+    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
+
+    ta = 6;
+    ws = 7;
+
+    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
+    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
+
+    ta = 6;
+    ws = 9;
+
+    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
+    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
+
+    ta = 6;
+    ws = 11;
+
+    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
+    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
+
+    ta = 10;
+    ws = 5;
+
+    WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
+    cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
+
     ta = 10;
     ws = 7;
 
@@ -82,4 +85,5 @@ int main() {
 
     WCI = k1 +(0.6125 * ta) - (k2 *(pow(ws, 0.16))) + (k3 * ta * pow(ws, 0.16));
     cout << ta << "(degrees C)" << setfill(' ') << setw(25) << ws << "(km/h)" << setfill(' ') << setw(15) << WCI << "(degrees C)" << endl;
+    
 }
--- homework/hw5/modulo.cpp
+++ homework/hw5/modulo.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main() {
     
+    /* this would be much easier to solve with the use of bools and arrays but i am not 100% firmilar with the syntax in c++ yet but i am looking into it*/
+    
+    
     int N, a, modulo, total, count = 0, f5, f6;
     int b = 42;
     int total_sub_input = 0;
