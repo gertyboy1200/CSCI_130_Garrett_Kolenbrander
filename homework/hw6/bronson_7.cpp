c5c299a4f6667a2ab143915b91378580aeb30249
gertyboy1200 <garrett.kolenbrander@gmail.com>
2022-10-03 02:39:56 -0600

    hw6🏁
    final files for hw6

--- homework/hw6/bronson_7.cpp
+++ homework/hw6/bronson_7.cpp
@@ -3,21 +3,26 @@ using namespace std;
 
 double power_gen();
 int main() {
-
     cout << " the watts generated is: "<< power_gen();
-
 }
 
 double power_gen() {
 
     double power, flow, distance;
-    //P =ρ × Q × H × g
+    const double density = 997;
+    const double g = 9.81;
+
     cout << "enter the flow rate in m^3/2: ";
     cin >> flow;
     cout << "enter the head distance in m: ";
     cin >> distance;
-    const double density = 997;
-    const double g = 9.81;
+    
     power = flow * g * density * distance;
+    
     return power;
 }
+
+/*
+part one: 2.73856 e+06
+part two: 5.86834 e+06
+*/
