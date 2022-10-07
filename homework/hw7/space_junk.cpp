bd19dcc51f38f4a1cd257e194c8d37715b7dec5a
gertyboy1200 <garrett.kolenbrander@gmail.com>
2022-10-07 09:05:12 -0600

    hw7🚧

--- homework/hw7/space_junk.cpp
+++ homework/hw7/space_junk.cpp
@@ -2,11 +2,18 @@
 
 using namespace std;
 
+int day_of_least_junk(int N);
+
 int main() {
 
-    int N, day, loop_var, lowest_value;
-    int array_place = 0;
+    int N;
     cin>>N;
+    cout << day_of_least_junk(N) << endl;
+}
+
+int day_of_least_junk(int N) {
+    int day, loop_var, lowest_value;
+    int array_place = 0;
     int space_junk[N];
     while (array_place < N) {
         cin >> space_junk[array_place];
@@ -15,16 +22,14 @@ int main() {
     lowest_value = space_junk[0];
     array_place = 0;
     while (array_place < N) {
-        cout << "array place: " << array_place << endl;
         if(lowest_value > space_junk[array_place]){
             lowest_value =space_junk[array_place];
             day = array_place;
         } 
         array_place++;
-        cout << "lowest value: " << lowest_value << endl;
     }
     if (space_junk[0] == lowest_value) {
         day = 0;
     }
-    cout << day << endl;
+    return day;
 }
