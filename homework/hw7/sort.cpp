e587763808e08bc1ca04a33beef18099842a8959
gertyboy1200 <garrett.kolenbrander@gmail.com>
2022-10-12 15:05:25 -0600

    hw7🚧

--- homework/hw7/sort.cpp
+++ homework/hw7/sort.cpp
@@ -2,44 +2,52 @@
 
 using namespace std;
 
-int main() 
-{ 
-    int N, C, array_place = 0;
-    cout << "enter n: ";
-    cin >> N;
-    int cipher[N];
-    cout << "test" << endl;
-    while (array_place < N) {
-        cin >> cipher[array_place];
-        array_place++;
+int countDistinct(int cipher[], int N){
+   int i, j, count = 1;
+   for (i = 1; i < N; i++){
+      for (j = 0; j < i; j++){
+         if (cipher[i] == cipher[j]){
+            break;
+         }
+      }
+      if (i == j){
+         count++;
+      }
+   }
+   return count;
+}
+void values_arr(int cipher[], int N) {
+    int array_place = 0;
+    int frequency[countDistinct(cipher, N)];
+    for (int i = 0; i < N; i++ ){
+        int j;
+        for (j = 0; j < i; j++){
+            if (cipher[i] == cipher[j])
+            break;
+        } 
+        if (i == j){
+            frequency[array_place] = cipher[i];
+            array_place++;
+        }
+
+    }
+    int length = sizeof(frequency) / sizeof(frequency[0]);
+    for(int x = 0; x < length; x++) {
+        cout << frequency[x] << " ";
     }
 
-    int visited[N];
-
-    for(int i=0; i<N; i++){
-
-        if(visited[i]!=1){
-           int count = 1;
-           for(int j=i+1; j<N; j++){
-              if(cipher[i]==cipher[j]){
-                 count++;
-                 visited[j]=1;
-              }
-            }
-
-            cout<<cipher[i]<<" occurs at "<<count<<" times "<<endl;
-         }
-     }
-
-    return 0; 
 }
 
-/*int main() {
-
-    int i = 0;
-
-    while (i <= 1000) {
-        cout << i << endl;
-        i++;
+int main(){
+    int C, N, index = 0, val;
+    cin >> N;
+    int cipher[N];
+    while (index < N) {
+        cin >> cipher[index];
+        index++;
     }
-}*/
+    values_arr(cipher, N);
+
+
+   return 0;
+}
