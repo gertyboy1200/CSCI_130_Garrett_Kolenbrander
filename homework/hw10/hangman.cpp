59d7d8307cb8b735c285a69bde5fcf45a3d0c495
gertyboy1200 <garrett.kolenbrander@gmail.com>
2022-10-29 18:48:46 -0600

    hw10

---
+++ homework/hw10/hangman.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+ 
+using namespace std; 
+
+int main() {
+
+    string word, alphabet;
+    cin >> word >> alphabet;
+ 
+    int word_length = word.length();
+    int alphabet_length = alphabet.length();
+    int alphabet_index = 0, win = 0, lose = 0, compare;
+
+    char word_array[word_length + 1];
+    char alphabet_array[alphabet_length + 1];
+ 
+    strcpy(word_array, word.c_str());
+    strcpy(alphabet_array, alphabet.c_str());
+
+    for (int j = 0; j < alphabet_length; j++) {
+        compare = win;
+        for (int i = 0; i < word_length; i++) {
+            if (word_array[i] ==  alphabet_array[j]) {
+                win++;
+            }
+        }
+        if (win == word_length) {
+            cout << "WIN";
+            return 0;
+        } else if(lose == 10){
+            cout << "LOSE";
+            return 0;
+        } else if (compare == win) {
+            lose++;
+        }
+    }
+
+    return 0;
+}
