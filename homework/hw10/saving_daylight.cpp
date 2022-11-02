60739026e9403ede477ec83f43bd91ee469ddb30
gertyboy1200 <garrett.kolenbrander@gmail.com>
2022-11-02 00:12:02 -0600

    hw10🏁
    final files for hw10

--- homework/hw10/saving_daylight.cpp
+++ homework/hw10/saving_daylight.cpp
@@ -5,11 +5,38 @@ using namespace std;
 int main () {
     int time, minutes;
 
-    cin >> time;
+    /*cin >> time;
     cin.ignore(); 
     cin >> minutes;
     cout << time << minutes;
-    cout << 2.0/3;
+    cout << 2 /3;*/
+
+
+  /*string line;
+  while (!line.empty()){
+    cin >> time;
+    cout << line << "\n";}
+*/
+
+bool empty_line_found = false;
+    string month;
+    int day, year, sunrise_hours, sunrise_minutes, sunset_hours, sunset_minutes;
+
+while(getline(cin, month) && !month.empty())
+{
+    cout << month << "\n";
+
+  if( sunset_minutes > 0 )
+  {
+  }
+  else
+  {
+    empty_line_found = true;
+    // otherwise, we have a line with data in it.
+    //  process the data here
+    //  (ie:  do whatever you're supposed to be doing with these lines as you read them)
+  }
+}
 
     return 0;
 }
